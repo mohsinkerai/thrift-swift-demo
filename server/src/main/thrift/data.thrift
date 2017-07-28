993e8426297ee5a013d8d4658dea0df96a8f4a12
Mohsin Mansoor Kerai <mohsin.kerai@gmail.com>
2017-07-28 13:14:32 +0500

    Fixing Phase of Execution for Plugin

--- server/src/main/thrift/data.thrift
+++ server/src/main/thrift/data.thrift
@@ -1,9 +1,15 @@
-namespace java.swift com.learning.thrift.generated
-namespace java com.learning.thrift.generated2
+namespace java.swift com.learning.thrift.files
+namespace java com.learning.thrift.files
 
 
 
+struct IntegerDTO {
+  1:  i32 value;
+  2:  i32 nullValue;
+}
 
 service CalculationService {
   i32 add(1:  i32 arg0, 2:  i32 arg1);
+  i32 addObject(1:  IntegerDTO arg0, 2:  IntegerDTO arg1);
+  IntegerDTO addObjectReturnObject(1:  IntegerDTO arg0, 2:  IntegerDTO arg1);
 }
